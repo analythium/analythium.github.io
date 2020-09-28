51e01cadf23f55b003f75643c7025d0b3a92a901
Peter Solymos <psolymos@gmail.com>
2020-09-27 21:15:48 -0600

    Meta title change

--- src/pages/index.js
+++ src/pages/index.js
@@ -56,7 +56,7 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Rock solid and open source analytics at your service <head />">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
